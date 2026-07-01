08b5621b951caf040fb26b938f122c55201be854
Mohammed-kordeya <137262670+Mohammed-kordeya@users.noreply.github.com>
2026-07-02 00:12:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+    origin: "*"
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is working 🚀");
+    res.send("Server is working 🚀");
 });
 
 app.post("/chat", (req, res) => {
-  res.json({ message: "working" });
+    const message = req.body.message;
+
+    return res.json({
+        reply: "أنت قلت: " + message
+    });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server running on", PORT);
 });
